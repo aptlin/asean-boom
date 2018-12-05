d8c1ea687a2e93f9c15783edfcb4359d07e5b544
sdll <shura.sdll@gmail.com>
2018-12-05 08:24:34 +0300

    initial commit

---
+++ components.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Box = styled.div`
+  display: table-cell;
+  width:50%;
+`;
+
+export const Title = styled.h1`
+  background: #fff;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  width: 100%;
+`;
+
+export const Source = styled.h1`
+  color: #dcdcdc;
+  font-size: 1em;
+  text-align: left;
+  position: absolute;
+  margin-left: 1em;
+  top: 90%;
+  width: 100%;
+`;
